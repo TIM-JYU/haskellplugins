8af48efcb848167c027f8d8d459db779df8640b3
Ville Tirronen <ville.tirronen@jyu.fi>
2015-08-03 12:37:25 +0300

    WIP

--- ExamplePlugin.hs
+++ ExamplePlugin.hs
@@ -14,12 +14,12 @@ import qualified Data.Text.Lazy as LT
 -- To see the rendered plugin then open localhost:8000/index.html in browser.
 -- When done, Ctrl-C will quit the plugin and return you to ghci. 
 
-toyPlugin :: Plugin (State LT.Text) (Input LT.Text) (Web Value,Save LT.Text)
+toyPlugin :: Plugin (State LT.Text) (Input LT.Text,State LT.Text) (Web Value,Save LT.Text)
 toyPlugin = let
     render (State txt) = return ("<p>This is a plugin</p><span id='thetextfield'>"<>txt<>"</span>\
                                \<button onclick=\"refresh(this,'kissa')\">Change</button>")
-    update (Input txt) = do
-        new <- render (State txt)
+    update (Input txt,State t) = do
+        new <- render (State (txt<>t))
         return (Web (object ["html" .= new])
              ,Save txt)
     requirements = [JS "/stdlib.js",JS "https://code.jquery.com/jquery-2.1.4.min.js"]

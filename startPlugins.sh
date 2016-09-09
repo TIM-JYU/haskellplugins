de1988175016ef0cb3becd30f17ed10211dcf7bb
Vesa Lappalainen <vesal@jyu.fi>
2016-09-09 18:30:34 +0300

    mmcq styles

---
+++ startPlugins.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# This is used to (re)start all Haskell plugins. Note that the ports used here must match tim registry
+#set -euo pipefail
+#IFS=$'\n\t'
+
+dockername="haskellplugins2"
+dockerOptions="--name $dockername --net=timnet \
+   -v /opt/tim/timApp/modules/Haskell/.cabal-sandbox/bin/:/hbin\
+   -v /opt/timb/timApp/modules/Haskell2/:/Haskell\
+   -v /opt/timb/timApp/modules/Haskell2/startAll.sh:/startAll.sh\
+   -p 57000:5001\
+   -p 58000:5002\
+   -p 59000:5003\
+   -p 60000:5004\
+   timimages/haskellrun  /bin/bash"
+
+
+docker stop haskellplugins2 > /dev/null 2>&1
+docker rm haskellplugins2 > /dev/null 2>&1
+
+if [ "$1" = "i" ]
+then
+    # interactive
+    docker run  --rm=true  -t -i $dockerOptions
+else
+    docker run -d $dockerOptions -c '/startAll.sh ; /bin/bash'
+fi

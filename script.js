53bbdf9e00fe31545f68aec08edf513b95eff28e
Mika Lehtinen <mikkalle@student.jyu.fi>
2018-08-21 12:29:01 +0300

    Use .then() instead of .success(), format files

--- script.js
+++ script.js
@@ -1,38 +1,47 @@
-function standardDirective(template,extract) {
- return function() {
-    return {
-      restrict: 'E',
-      templateUrl: function(elem,attrs) {
-                    return $sce.trustAsUrl(elem.parent().attr('data-plugin')+"/"+template);
-                   }, 
-      replace: true,
-      scope: {},
-      controller: function($scope, $element, $http) {
-        $scope.ident     = $element.parent().attr("id"); 
-        $scope.content = JSON.parse($element.attr("data-content"));
-        $scope.plugin = $element.parent().attr("data-plugin");
-        $scope.submit = function () {
-            var message = {input:extract($scope)};
-            var localState  = JSON.parse($element.parent().attr("data-state")  || "null");
-            var localMarkup = JSON.parse($element.parent().attr("data-markup") || "null");
-            if (localState!==null) {message.state = localState};
-            if (localMarkup!==null) {message.markup = localMarkup};
-            $http({method:'PUT'
-                  ,url:$scope.plugin+"/"+$scope.ident+"/answer/"
-                  ,data:message})
-             .success(function(data){
-                  $scope.content = data.web
-                  console.log(["data",$scope.content]);
-                 })
-             .error(function(data,status,hdrs,cfg){
-                  alert(["error",data,status,hdrs,cfg]);
-                 });
-            };
-       }
+function standardDirective(template, extract) {
+    return function () {
+        return {
+            restrict: 'E',
+            templateUrl: function (elem, attrs) {
+                return $sce.trustAsUrl(elem.parent().attr('data-plugin') + "/" + template);
+            },
+            replace: true,
+            scope: {},
+            controller: function ($scope, $element, $http) {
+                $scope.ident = $element.parent().attr("id");
+                $scope.content = JSON.parse($element.attr("data-content"));
+                $scope.plugin = $element.parent().attr("data-plugin");
+                $scope.submit = function () {
+                    var message = {input: extract($scope)};
+                    var localState = JSON.parse($element.parent().attr("data-state") || "null");
+                    var localMarkup = JSON.parse($element.parent().attr("data-markup") || "null");
+                    if (localState !== null) {
+                        message.state = localState
+                    }
+                    if (localMarkup !== null) {
+                        message.markup = localMarkup
+                    }
+                    $http({
+                        method: 'PUT'
+                        , url: $scope.plugin + "/" + $scope.ident + "/answer/"
+                        , data: message
+                    })
+                        .then(function (resp) {
+                            var data = resp.data;
+                            $scope.content = data.web;
+                            console.log(["data", $scope.content]);
+                        }, function (err) {
+                            alert(["error", err]);
+                        });
+                };
+            }
+        }
     }
-  }
 }
+
 angular.module('MCQ', [])
-  .directive('mcq', standardDirective("MCQTemplate.html"
-                                     , function(scope){return scope.userSelection;}));
+    .directive('mcq', standardDirective("MCQTemplate.html"
+        , function (scope) {
+            return scope.userSelection;
+        }));
 
